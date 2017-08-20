0f813b9ae08923a5b6a2e115d60e33d49cc04b18
victor-iyiola <javafolabi@gmail.com>
2017-08-20 12:34:59 +0100

    Custom template setupj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-victor',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'my app-victor app!'
+  name = 'Victor'
+  age = 22
+  skill = 'Programming'
 }
